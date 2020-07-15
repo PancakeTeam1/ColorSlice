aaa49ebf2c72709e3b08fb960f7d2d22ef31a099
Alexandr <sasha.abramov2979159@gmail.com>
2020-07-15 16:29:16 +0300

    Animation for Hammer

---
+++ Assets/Scripts/HammerHit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HammerHit : MonoBehaviour
+{
+    public Animator Animator;
+    
+    public void Hit()
+    {
+        Animator.SetTrigger("StartHit");    
+    }
+}

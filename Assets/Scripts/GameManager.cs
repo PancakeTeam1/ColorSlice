1400a600647dfb80124c2c33235fa3315596e866
FadBy <marat.safin.2002@mail.ru>
2020-07-15 17:11:17 +0300

    Перемещение камеры в режиме ленты

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Vector3 currentCube;
+    private Mode mode = Mode.Tape;
+    enum Mode
+    {
+        Canvas,
+        Tape
+    }
+
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Update()
+    {
+        if (mode == Mode.Tape)
+        {
+
+        }
+    }
+
+}

c2bef51cd78857b6c80c063484f112c8ed77a164
Artem <artyom201004@gmail.com>
2020-07-16 18:39:03 +0300

    16Commit

--- Assets/Scripts/Game/Managers/InGameImageLoader.cs
+++ Assets/Scripts/Game/Managers/InGameImageLoader.cs
@@ -6,24 +6,27 @@ public class InGameImageLoader : MonoBehaviour
 {
     public Color[,] NotNormalizedColors;
     public List<Color> NormalizedColors;
-
-    public Texture2D Texture;
-    public int RowSize;
-    public int ColumnSize;
-    public int OffsetX;
-    public int OffsetY;
-    public int PaddingX;
-    public int PaddingY;
-
+    public List<Texture2D> Texture;
+    public List<int> RowSize;
+    public List<int> ColumnSize;
+    public List<int> OffsetX;
+    public List<int> OffsetY;
+    public List<int> PaddingX;
+    public List<int> PaddingY;
     public float MaxDifference = 0.15f;
 
-    public void CreatePicture()
-    {
-        NotNormalizedColors = AnalysePixel.Analyse(Texture, RowSize, ColumnSize, OffsetX, OffsetY, PaddingX, PaddingY);
+    public int NumberOfCurrentPicture = 0;
 
-        for(int row = 0; row < RowSize; row++)
+    public void CreatePicture(int NumberOfPicture)
+    {
+        int CurrentRowSize = RowSize[NumberOfPicture];
+        int CurrentColumnSize = ColumnSize[NumberOfPicture];
+
+        NotNormalizedColors = AnalysePixel.Analyse(Texture[NumberOfPicture], CurrentRowSize, CurrentColumnSize, OffsetX[NumberOfPicture], OffsetY[NumberOfPicture], PaddingX[NumberOfPicture], PaddingY[NumberOfPicture]);
+
+        for(int row = 0; row < CurrentRowSize; row++)
         {
-            for(int column = 0; column < ColumnSize; column++)
+            for(int column = 0; column < CurrentColumnSize; column++)
             {
                 // Check is it new color or not
                 bool foundBaseColor = false;
@@ -61,11 +64,11 @@ public class InGameImageLoader : MonoBehaviour
         return D;
     }
 
-    public void DebugColors()
+    public void DebugColors(int NumberOfPicture)
     {
-        for (int row = 0; row < RowSize; row++)
+        for (int row = 0; row < RowSize[NumberOfPicture]; row++)
         {
-            for (int column = 0; column < ColumnSize; column++)
+            for (int column = 0; column < ColumnSize[NumberOfPicture]; column++)
             {
                 Debug.Log(NotNormalizedColors[row, column]);
             }

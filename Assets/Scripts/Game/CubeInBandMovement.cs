8d04cb4ae844481f6078f2642ebfffcc031afc4d
FadBy <marat.safin.2002@mail.ru>
2020-07-17 12:37:04 +0300

    ALmost band Ready

--- Assets/Scripts/Game/CubeInBandMovement.cs
+++ Assets/Scripts/Game/CubeInBandMovement.cs
@@ -4,5 +4,17 @@ using UnityEngine;
 
 public class CubeInBandMovement : MonoBehaviour
 {
+    public float speed = 15f;
+    private BandGenerator generator;
 
+    private void Awake()
+    {
+        generator = transform.parent.GetComponent<BandGenerator>();
+    }
+
+    private void Update()
+    {
+        Vector3 changed = (generator.endPoint - generator.startPoint).normalized * speed * Time.deltaTime;
+        transform.Translate(changed);
+    }
 }
---
+++ Assets/Scripts/Game/Managers/BandGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BandGenerator : MonoBehaviour
+{
+    public GameObject PrefabCube;
+    public float distance;
+
+    [HideInInspector]
+    public Vector3 startPoint;
+    [HideInInspector]
+    public Vector3 endPoint;
+    private Vector3 lastCubePos;
+    private GameObject lastCube;
+    private float currentDistance;
+
+    private Pooler pooler;
+    private CubeInBandMovement cube;
+
+    private void Awake()
+    {
+        startPoint = transform.Find("StartPoint").position;
+        endPoint = transform.Find("EndPoint").position;
+        cube = PrefabCube.GetComponent<CubeInBandMovement>();
+        
+    }
+
+    private void Start()
+    {
+        pooler = Pooler.instance;
+    }
+
+    private void Update()
+    {
+        if (lastCube == null || currentDistance >= distance)
+        {
+            lastCube = pooler.SpawnFromPull(PrefabCube, startPoint, transform);
+            currentDistance = 0;
+            lastCubePos = startPoint;
+        }
+        else
+        {
+            currentDistance += (lastCube.transform.position - lastCubePos).magnitude;
+        }
+    }
+}

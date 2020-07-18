93dbd2e9177221625a1bc8651f6153e7a606199d
Alexandr <sasha.abramov2979159@gmail.com>
2020-07-18 14:33:26 +0300

    Main menu Images

--- Assets/Scripts/Game/HammerHit.cs
+++ Assets/Scripts/Game/HammerHit.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class HammerHit : MonoBehaviour
 {
-    public Animator Animator;
-    
+    private Animator Animator;
+
+    private void Start()
+    {
+        Animator = this.GetComponent<Animator>();
+    }
+
     public void Hit()
     {
         Animator.SetTrigger("StartHit");    

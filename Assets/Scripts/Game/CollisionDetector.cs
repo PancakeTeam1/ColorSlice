e744702706408c3a31888ed0c1916310d0edb09c
Artem <artyom201004@gmail.com>
2020-07-19 18:00:40 +0300

    half of cube movement making

---
+++ Assets/Scripts/Game/CollisionDetector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Monetization;
+
+public class CollisionDetector : MonoBehaviour
+{
+    private GameManager gameManager;
+
+    private void Start()
+    {
+        gameManager = GameManager.Instance;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("collision!");
+        other.GetComponent<CubeInBandMovement>().enabled = false;
+        CubeToCanvasMovement cube = other.GetComponent<CubeToCanvasMovement>();
+        cube.statement = true;
+        cube.place = gameManager.CurrentCube;
+    }
+}
--- Assets/Scripts/Game/CubeInBandMovement.cs
+++ Assets/Scripts/Game/CubeInBandMovement.cs
@@ -22,13 +22,4 @@ public class CubeInBandMovement : MonoBehaviour
             gameObject.SetActive(false);
         }
     }
-
-    public void OnHitbuttonClick()
-    {
-        if(this.transform.position.x >= Camera.main.transform.position.x-0.5f && this.transform.position.x <= Camera.main.transform.position.x + 0.5f)
-        {
-            movement.statement = true;
-            movement.place = Camera.main.transform.position;
-        }
-    }
 }

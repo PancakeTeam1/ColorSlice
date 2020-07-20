7d27ee83c769ebd4eea3b7209a3dee04c2f6ca6c
Alexandr <sasha.abramov2979159@gmail.com>
2020-07-20 17:25:22 +0300

    Fucking bug fixed

--- Assets/Scripts/Game/CubeToCanvasMovement.cs
+++ Assets/Scripts/Game/CubeToCanvasMovement.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class CubeToCanvasMovement : MonoBehaviour
 {
     private bool ShouldMoveToPosition = false;
 
-    public float Speed = 1f;
+    public float Speed = 5f;
     public bool statement;  // when should we move our cube to place
     public Vector3 place;  // place, where should we move our cube
 
@@ -28,21 +29,15 @@ public class CubeToCanvasMovement : MonoBehaviour
 
         if (ShouldMoveToPosition)
         {
-            this.transform.position = Vector3.Lerp(this.transform.position, place, Time.deltaTime * Speed);
+            transform.Translate((place - this.transform.position).normalized * Speed * Time.deltaTime);
         }
 
         if (Vector3.Distance(this.transform.position, place) < 0.05f && ShouldMoveToPosition)
         {
-            Debug.Log("should move");
             ShouldMoveToPosition = false;
             this.gameObject.SetActive(false);
             gameManager.CurrentCube.PutCubeBand();
             //bandGenerator.isDeactivated = true;
         }
-    }
-
-    public void ShouldCubeMove(bool condition)
-    {
-        ShouldMoveToPosition = condition;
     }
 }
